76cefbe09179dfd8afb902da5e4f144ab49605f3
fudi500 <pawel.fudalewski@gmail.com>
2018-03-21 13:45:09 +0100

    initial commit

---
+++ src/main/java/pl/fudalewski/ATR/AirportTransferReservationApplication.java
@@ -0,0 +1,12 @@
+package pl.fudalewski.ATR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportTransferReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportTransferReservationApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/fudalewski/ATR/AirportTransferReservationApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.fudalewski.ATR;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AirportTransferReservationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
